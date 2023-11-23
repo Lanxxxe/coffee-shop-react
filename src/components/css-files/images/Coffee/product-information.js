3b46918361fc37e7b1bf6f46088f7083aab284fc
Charles Lance <lancegula05@gmail.com>
2023-11-24 00:03:49 +0800

    Create Produc List

---
+++ src/components/css-files/images/Coffee/product-information.js
@@ -0,0 +1,107 @@
+// Cold Drinks
+import ChocoHazelnut from './Cold/Choco-Hazelnut.jpg';
+import ChocoKisess from './Cold/Choco-Kisses-Iced-Coffee.jpg';
+import ChocoMilk from './Cold/Chocolate-Milk-Chocolate.jpg';
+import DarkChocolate from './Cold/Dark-Chocolate-Coffee.jpg';
+import Frappe from './Cold/Dark-Chocolate-Coffee.jpg';
+import IcedMocha from './Cold/Iced-Cafe-Mocha.jpg';
+import CaramelMacchiato from './Cold/Iced-Caramel-Macchiato.jpg';
+import Coffee from './Cold/Iced-Coffee.jpg';
+import Late from './Cold/Iced-Lattee.jpg';
+import StrawberryLate from './Cold/Iced-Strawberry-Latte.jpg';
+
+//Hot Drinks
+import Americano  from './Hot/Americano-Coffee.jpg';
+import BlackCoffee from './Hot/Black-Coffee.jpg';
+import BrownCoffee from './Hot/Brown-Coffee.jpg';
+import Cappucino from './Hot/Cappuccino.jpg';
+import CaramelCoffee from './Hot/Caramel-Hot-Coffee.jpg';
+import ChocolateCoffee from './Hot/Chocolate-Hot-Coffee.jpg';
+import EspressoMacchiato from './Hot/Espresso-macchiato.jpeg';
+import LateSmall from './Hot/latte-small.jpg';
+import Marocchino from './Hot/Marocchino.jpg';
+import NutellaCoffee from './Hot/Nutella-Hot-Coffee.jpg';
+
+
+//Cookies
+import TwoTone from './Cookies/Black-White-Cookies.jpg';
+import ChocoFudge from './Cookies/Chocolate-Fudge-Cookies.jpg';
+import ClassicPeanutButter from './Cookies/Classic-Peanut-Butter-Cookies.jpg';
+import Gingerbread from './Cookies/Gingerbread-Cookies.jpg';
+import StrawberryCheesecake from './Cookies/strawberry-cheesecake-cookies.jpg';
+import SweetendCondensed from './Cookies/strawberry-cheesecake-cookies.jpg';
+import Unesweetend from './Cookies/Unsweetend-Cookies.jpg';
+
+let products = [
+    {
+        id : 1,
+        name : "Choco Hazelnut",
+        image : ChocoHazelnut,
+        catergory : 'Cold',
+        price : 150,
+    },
+    {
+        id : 2,
+        name : "Chocolate Kisses Iced Coffee",
+        image : ChocoKisess,
+        catergory : 'Cold',
+        price : 170,
+    },
+    {
+        id : 3,
+        name : "Chocolate Milk Iced Coffee",
+        image : ChocoMilk,
+        catergory : 'Cold',
+        price : 190,
+    },
+    {
+        id : 4,
+        name : "Dark Chocolate Coffee",
+        image : DarkChocolate,
+        catergory : 'Cold',
+        price : 130,
+    },
+    {
+        id : 5,
+        name : "Frappe",
+        image : Frappe,
+        catergory : 'Cold',
+        price : 100,
+    },
+    {
+        id : 6,
+        name : "Iced Cafe Mocha",
+        image : IcedMocha,
+        catergory : 'Cold',
+        price : 120,
+    },
+    {
+        id : 7,
+        name : "Iced Caramel Macchiato",
+        image : CaramelMacchiato,
+        catergory : 'Cold',
+        price : 150,
+    },
+    {
+        id : 8,
+        name : "Iced Coffee",
+        image : Coffee,
+        catergory : 'Cold',
+        price : 89,
+    },
+    {
+        id : 9,
+        name : "Iced Late",
+        image : Late,
+        catergory : 'Cold',
+        price : 100,
+    },
+    {
+        id : 10,
+        name : "Iced Strawberry Late",
+        image : StrawberryLate,
+        catergory : 'Cold',
+        price : 160,
+    }
+    
+]
